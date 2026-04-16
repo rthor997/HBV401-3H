33c8c2bd7cf309c8d0956ac3b65de80b654ada49
fannarfent <fannarorn17@gmail.com>
2026-04-16 16:37:55 +0000

    Bæta við hotel

--- src/main/java/org/example/hbv4013h/model/Hotel.java
+++ src/main/java/org/example/hbv4013h/model/Hotel.java
@@ -1,4 +1,16 @@
 package org.example.hbv4013h.model;
 
 public class Hotel {
+    private String hotelID;
+    private final String name;
+    private final String location;
+    private Boolean allowsPets;
+    private List<Room> rooms;
+    public Hotel(String name, String location, Boolean allowPets,List<Room> rooms){
+        this.name = name;
+        this.location = location;
+        this.allowsPets = allowPets;
+    }
+    
+
 }
