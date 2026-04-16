56b79df14db6fd1e964f5cf282dc7309383854dc
Runar Thor <rthor997@gmail.com>
2026-04-16 10:43:56 +0000

    Booking.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module org.example.hbv4013h {
 
     opens org.example.hbv4013h to javafx.fxml;
     exports org.example.hbv4013h;
+    exports org.example.hbv4013h.ui;
+    opens org.example.hbv4013h.ui to javafx.fxml;
 }
--- src/main/java/org/example/hbv4013h/model/Booking.java
+++ src/main/java/org/example/hbv4013h/model/Booking.java
@@ -1,4 +1,33 @@
 package org.example.hbv4013h.model;
 
+
+import java.time.LocalDate;
+
 public class Booking {
+    private String bookingId;
+    private Customer customer;
+    private Room room;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private int guests;
+    private boolean lateCheckout;
+    private double price;
+
+
+    public Booking(String bookingId, Customer customer, Room room,
+                   LocalDate checkIn, LocalDate checkOut, int guests) {
+        this.bookingId = bookingId;
+        this.customer = customer;
+        this.room = room;
+        this.checkInDate = checkIn;
+        this.checkOutDate = checkOut;
+        this.guests = guests;
+        this.price = calculateTotalPrice();
+    }
+
+    public double calculateTotalPrice() {
+        long days = checkOutDate.toEpochDay() - checkInDate.toEpochDay();
+        return days * room.getPricePerDay(); // getPriceperday í room
+    }
+
 }
